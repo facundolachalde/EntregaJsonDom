5cd2f61382a507787003c6a77b087cab4d7ba1bf
Facu. L <faculachalde123@gmail.com>
2022-10-05 22:11:29 -0300

    entrega lib

--- js/scripts.js
+++ js/scripts.js
@@ -27,12 +27,20 @@ function addToCarritoItem(e){
 
 function addItemCarrito(newItem){
 
-  const alert = document.querySelector('.alert')
-
-  setTimeout( function(){
-    alert.classList.add('hide')
-  }, 2000)
-    alert.classList.remove('hide')
+  (async () => {
+    const {value: alert} = Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'Producto añadido al carrito',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  
+    setTimeout( function(){
+      alert.classList.add('hide')
+    }, 2000)
+      alert.classList.remove('hide')
+  })() 
 
   const InputElemnto = tbody.getElementsByClassName('input__elemento')
   for(let i =0; i < carrito.length ; i++){
@@ -102,12 +110,20 @@ function removeItemCarrito(e){
     }
   }
 
-  const alert = document.querySelector('.remove')
-
-  setTimeout( function(){
-    alert.classList.add('remove')
-  }, 2000)
-    alert.classList.remove('remove')
+  (async () => {
+    const {value: alert} = Swal.fire({
+      position: 'top-end',
+      icon: 'error',
+      title: 'Producto eliminado al carrito',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  
+    setTimeout( function(){
+      alert.classList.add('remove')
+    }, 2000)
+      alert.classList.remove('remove')
+  })() 
 
   tr.remove()
   CarritoTotal()
